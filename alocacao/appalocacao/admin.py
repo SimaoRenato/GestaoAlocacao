033ff59310b9b86288b198941e61fa692c1a5e52
SimaoRenato <57469051+SimaoRenato@users.noreply.github.com>
2022-01-30 15:36:07 -0300

    sincronizado com BD do workbench, criado visão de admin

--- alocacao/appalocacao/admin.py
+++ alocacao/appalocacao/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Alocacao
+from .models import Area
+from .models import Colaborador
+from .models import Contratos
+
+
+admin.site.register(Alocacao)
+admin.site.register(Area)
+admin.site.register(Colaborador)
+admin.site.register(Contratos)
