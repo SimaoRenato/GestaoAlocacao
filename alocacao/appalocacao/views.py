1d07a627622323a1f8c5e3e5b2d3206455346603
SimaoRenato <57469051+SimaoRenato@users.noreply.github.com>
2022-01-31 22:21:06 -0300

    Início de consultas e configuração de exibição.

--- alocacao/appalocacao/views.py
+++ alocacao/appalocacao/views.py
@@ -7,11 +7,23 @@ from .models import Colaborador
 
 
 def index(request):
-    total_colaboradores_cadastratos = len(Colaborador.objects.values())
-    total_colaboradores_ativos = len(Colaborador.objects.all().filter(ativo='sim'))
+    colab = Colaborador.objects.all()
+    total_colaboradores_cadastratos = len(colab)
+    total_colaboradores_ativos = len(colab.filter(ativo='sim'))
     output = 'total de colaboradores cadastrados: {}'.format(total_colaboradores_cadastratos)
     output = output + "<br>" + " total de colaboradores ativos: {}".format(total_colaboradores_ativos)
+    output = output + "<br><br> <h1> Lista de colaboradores cadastrados: </h1>"
+
+    for nome in colab:
+        output = output + "<br>" + nome.nomecolaborador
+
+    output = output + "<br><br> <h1> Lista de colaboradores ativos:</h1>"
+
+    for i in colab.filter(ativo='sim'):
+        output = output + "<br>" + i.nomecolaborador
+
     return HttpResponse(output)
+
 
 def newAloc(request,name):
     return HttpResponse("Aqui poderá ser cadastrada uma nova alocação para %s" % name)
