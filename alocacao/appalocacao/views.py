e733e59f9472a54d95dff5266de6dd70785790aa
SimaoRenato <57469051+SimaoRenato@users.noreply.github.com>
2022-01-31 22:06:42 -0300

    editado o index view para consultar número de colaboradores

--- alocacao/appalocacao/views.py
+++ alocacao/appalocacao/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Colaborador
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at alocaco index.")
+    total_colaboradores_cadastratos = len(Colaborador.objects.values())
+    total_colaboradores_ativos = len(Colaborador.objects.all().filter(ativo='sim'))
+    output = 'total de colaboradores cadastrados: {}'.format(total_colaboradores_cadastratos)
+    output = output + "<br>" + " total de colaboradores ativos: {}".format(total_colaboradores_ativos)
+    return HttpResponse(output)
+
+def newAloc(request,name):
+    return HttpResponse("Aqui poderá ser cadastrada uma nova alocação para %s" % name)
