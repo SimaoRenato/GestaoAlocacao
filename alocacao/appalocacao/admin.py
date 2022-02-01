91f6a3cfbaf728dbcde2d2dc96973e40b5abd00d
SimaoRenato <57469051+SimaoRenato@users.noreply.github.com>
2022-01-31 21:13:54 -0300

    Corrigido erro do models.py ajustando para UTF8 no notepad++;  Ajustado a visibilidade das tabelas no admin.py; definido os methodos string das classes

--- alocacao/appalocacao/admin.py
+++ alocacao/appalocacao/admin.py
@@ -6,9 +6,16 @@ from .models import Alocacao
 from .models import Area
 from .models import Colaborador
 from .models import Contratos
-
+from .models import Tipoalocacao
+from .models import Valorhora
+from .models import Projetos
+from .models import Equipe
 
 admin.site.register(Alocacao)
 admin.site.register(Area)
 admin.site.register(Colaborador)
 admin.site.register(Contratos)
+admin.site.register(Projetos)
+admin.site.register(Tipoalocacao)
+admin.site.register(Valorhora)
+admin.site.register(Equipe)
